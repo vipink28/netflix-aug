983e4b0edd2823006e0661818fc704cfb362fd0a
vipink28 <vipinkalyan28@gmail.com>
2023-10-17 10:37:19 +0530

    tv slice created.

---
+++ src/features/tv/tvSlice.js
@@ -0,0 +1,43 @@
+// initial state
+
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from '../../helper/axios';
+import { requests } from "../../helper/requests";
+
+
+const initialState = {
+    netflixOriginals:{
+        status:"idle",
+        data:null,
+        error: null
+    }
+}
+
+export const fetchNetflixOriginals = createAsyncThunk(
+    'tv/fetchNetflixOriginals',
+    async () => {
+      const response = await axios.get(requests.getNetflixOriginals);
+      return response.data;
+    }
+  );
+
+
+
+export const tvSlice = createSlice({
+  name: "tv",
+  initialState,
+  reducers:{},
+  extraReducers:(builder)=>{
+    builder
+    .addCase(fetchNetflixOriginals.pending, (state, action)=>{
+        state.netflixOriginals.status = "loading";
+    })
+    .addCase(fetchNetflixOriginals.fulfilled, (state, action)=>{
+        state.netflixOriginals.status = "success";
+        state.netflixOriginals.data = action.payload;
+    })
+  }
+})
+
+
+export default tvSlice.reducer;
--- src/helper/requests.js
+++ src/helper/requests.js
@@ -1,5 +1,7 @@
-const API_KEY = "0d8ab7cff2692bd014bb25fca16d7158";
+var API_KEY = "0d8ab7cff2692bd014bb25fca16d7158";
 
-const requests = {
+export const requests = {
     getNetflixOriginals: `discover/tv?api_key=${API_KEY}&with_networks=213`
 }
+
+
